f3f55c61d622f02dff7ebcb1fe5d56c7602f282a
Adrián Agudo Bruno <56310444+aagudobruno@users.noreply.github.com>
2020-05-25 15:19:20 +0200

    Add files via upload

--- Proyectofinal/src/ventanas/login.java
+++ Proyectofinal/src/ventanas/login.java
@@ -10,10 +10,10 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import proyectofinal.InicioSesion;
 import proyectofinal.conectarBBDD;
+
 
 /**
  *
